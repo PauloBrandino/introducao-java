fcfbe1eb45a4bc976b44f452ad360ab8e82ed4af
PauloBrandino <contatobrandino@gmail.com>
2023-09-11 13:31:55 -0300

    Metodos

---
+++ Classes-1/src/metodos/Metodos.java
@@ -0,0 +1,14 @@
+package metodos;
+
+public class Metodos {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public void imprimir(String texto) {
+        System.out.println(texto);
+    }
+
+    private void metodoPrivado() {}
+}
